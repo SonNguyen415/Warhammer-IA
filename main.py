15abaea8a9b62d70a5de76fe2e533ff991c87832
Son Nguyen <snguyen21@ssis.edu.vn>
2020-08-18 21:26:22 +0700

    yes

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+import sys
+
+
+def delay_print(text):
+    for w in text:
+        sys.stdout.write(w)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+def print_intro():
+    gameIntro = open('intro', 'r')
+    introContent = gameIntro.read()
+    delay_print(introContent)
+    gameIntro.close()
+
+
+def start_menu():
+    print('hi')
+
+
+print_intro()
+start_menu()
