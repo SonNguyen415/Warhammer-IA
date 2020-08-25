22028dc15a3e1bb8871a8dc5f437b33dc74a083b
The_Lord_Inquisitor <Son.mofa415@gmail.com>
2020-08-25 08:12:39 +0700

    ok

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sys
 
 
 def delay_print(text):
+    # Code courtesy of stackOverflow
     for w in text:
         sys.stdout.write(w)
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.02)
 
 
 def print_intro():
@@ -17,10 +18,9 @@ def print_intro():
 
 
 def start_menu():
-    print('')
+    print(' ')
 
 
 print_intro()
 start_menu()
 exitKey = input('Press any key to exit: ')
-
