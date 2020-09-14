488fa46f31184e0e0e0bd42dc443a011157dc200
Son Nguyen <snguyen21@ssis.edu.vn>
2020-09-14 11:55:55 +0700

    Update type.py

--- type.py
+++ type.py
@@ -6,10 +6,12 @@ from content import *
 currCHAR = 0
 
 
+#Indent for aesthetic and readability
 def indent(space):
     return " " * space
 
 
+#Print one word at a time per sentence.
 def delay_print(text):
     # Code courtesy of stackOverflow
     for w in text:
@@ -17,24 +19,30 @@ def delay_print(text):
         sys.stdout.flush()
         time.sleep(0.02)
 
-
+        
+#Skipping lines
 def skip_line(line):
     print("\n" * line)
 
-
+    
+#Print the intro and wait 2 seconds
 def print_intro():
     # delay_print(introContent)
     print("Yes")
     time.sleep(2)
     intro.close()
 
-
+    
+#Load new game screen
 def new_game():
     skip_line(5)
+    global currChar
     name = input("Enter a name: ")
     insert_character(name, 1, get_id() + 1)
+    currChar = get_id()
 
-
+    
+#Allow loading game and change the current characterID
 def load_game():
     global currCHAR
     get_character_list()
@@ -45,7 +53,8 @@ def load_game():
         except ValueError:
             currCHAR = int(input("Please type in your character id properly: \n"))
 
-
+            
+#Load all the options a player can have in the menu
 def load_options():
     user_option = input("Choose your options: ")
     if user_option.lower() == "new game":
@@ -55,7 +64,8 @@ def load_options():
     else:
         load_options()
 
-
+        
+#Create menu and starting screen
 def render_menu():
     skip_line(40)
     delay_print("Welcome to Warhammer 40k. The grim dark future of humanity is at hand. \n"
