e650db3ee640b8afaf64738648dd4d5032c497f2
Son Nguyen <snguyen21@ssis.edu.vn>
2020-10-29 07:43:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from story import *
 
 currScene = 1
-
+gameContinue = True
 
 # Make the game
 print_intro()
 time.sleep(WAIT_TIME)
-while True:
+while gameContinue:
     Player = render_menu()
     if in_database(Player.charID):
         currScene = get_curr_progress(Player.charID)
     Player.check_stats()
     start_game()
     game_progress(currScene, Player)
-    end_game()
+    gameContinue = end_game()
